68c667053c9141a4200dab042fc0d8e8f2e545c0
Harsh-Agrawal-425 <131527639+Harsh-Agrawal-425@users.noreply.github.com>
2024-03-01 19:05:48 +0530

    Update 220425_cpp_assignment.cpp

--- 220425_cpp_assignment.cpp
+++ 220425_cpp_assignment.cpp
@@ -1,50 +1,11 @@
 //==============================================================================================================================
 /*
-README:
-
 Name: Harsh Agarwal
 Roll No: 220425
 Course: CS253
 Assignment 1
-
-1. Rental Limits Based on Record:
-   - "Good" record: Can rent up to 3 cars.
-   - "Average" record: Can rent up to 2 cars.
-   - "Poor" record: Can rent only 1 car.
-
-2. Car Conditions:
-   - Possible conditions are "good", "average", and "poor".
-
-3. Total Due Calculation:
-   - The 'total_due' is the sum of the net fine imposed and the rent of the car.
-
-4. Car Database:
-   - The car database file is named "cars.csv".
-   - Format: Car ID, Model, Condition, Rent Price, Rent Person Role (-1 if not rented, 0 for employee, 1 for customer), Rent Person ID (-1 if not rented), Due Date
-
-5. Customer Database:
-    - The customer database file is named "customers.csv".
-    - Format: Customer ID, Name, Password, Record (good, average, poor), Total Due, Number of Cars Rented, Car ID 1, Car ID 2, Car ID 3
-
-6. Employee Database:
-    - The employee database file is named "employees.csv".
-    - Format: Employee ID, Name, Password, Record (good, average, poor), Total Due, Number of Cars Rented, Car ID 1, Car ID 2, Car ID 3
-
-7. Manager Database:
-    - The manager database file is named "managers.csv".
-    - Format: Manager ID, Name, Password
-
-8. Program Exit:
-   - Please ensure to exit the program properly before running it again to prevent potential crashes.
-
-9. Contact:
-    - For any queries, please contact me at: 
-      - Email:harshag22@iitk.ac.in
-
 */
-//================================================================================================================================
-
-
+//==============================================================================================================================
 //HEADER FILES
 #include <iostream>
 #include <cstdlib>
